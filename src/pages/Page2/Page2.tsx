70c3e2e337478f970eaa82d8999aaece1c745c43
Manon Lavenier <manon.lavenier@gmail.com>
2022-06-21 12:04:28 +0200

    dice roll

--- src/pages/Page2/Page2.tsx
+++ src/pages/Page2/Page2.tsx
@@ -1,16 +1,38 @@
-import Typography from '@mui/material/Typography';
+import Dice from 'react-dice-roll';
 
-import Meta from '@/components/Meta';
-import { FullSizeCenteredFlexBox } from '@/components/styled';
+import './styles.css';
 
 function Page2() {
+  const dice1 = [
+    'https://imagizer.imageshack.com/img923/2142/7TcyCy.png',
+    'https://imagizer.imageshack.com/img923/1822/ST7vEp.png',
+    'https://imagizer.imageshack.com/img924/7038/jQWboH.png',
+    'https://imagizer.imageshack.com/img922/3003/wEsmUK.png',
+    'https://imagizer.imageshack.com/img924/3087/bJNg7v.png',
+    'https://imagizer.imageshack.com/img922/9082/GMPtl4.png',
+  ];
+
+  const dice2 = [
+    'https://imagizer.imageshack.com/img922/162/r9xIoH.png',
+    'https://imagizer.imageshack.com/img924/3162/GqMt6O.png',
+    'https://imagizer.imageshack.com/img923/3193/iQUCll.png',
+    'https://imagizer.imageshack.com/img924/9185/4LOkfJ.png',
+    'https://imagizer.imageshack.com/img923/9311/EriXlK.png',
+    'https://imagizer.imageshack.com/img922/1740/opwOil.png',
+  ];
+
   return (
-    <>
-      <Meta title="page 2" />
-      <FullSizeCenteredFlexBox>
-        <Typography variant="h3">Page 2</Typography>
-      </FullSizeCenteredFlexBox>
-    </>
+    <div className="App">
+      <h1>Dés virtuels des questions</h1>
+      <div className="dices">
+        <div className="diceOne">
+          <Dice size={200} faces={dice1} />
+        </div>
+        <div className="diceTwo">
+          <Dice size={200} faces={dice2} />
+        </div>
+      </div>
+    </div>
   );
 }
 
